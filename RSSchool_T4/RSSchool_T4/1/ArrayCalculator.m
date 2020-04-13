4f4b0f886a2f19fff1dee119b04badb718b9bbb3
TimofeyZykov <zikovtm@gmail.com>
2020-04-13 16:12:41 +0300

    fix:add solutions for first and third exercises

--- RSSchool_T4/RSSchool_T4/1/ArrayCalculator.m
+++ RSSchool_T4/RSSchool_T4/1/ArrayCalculator.m
@@ -2,7 +2,53 @@
 
 @implementation ArrayCalculator
 + (NSInteger)maxProductOf:(NSInteger)numberOfItems itemsFromArray:(NSArray *)array {
-    // good luck
-    return -1;
+    NSMutableArray *inputArray = [NSMutableArray arrayWithArray:array];
+    NSMutableArray *positiveArray = [[NSMutableArray alloc] init];
+    NSMutableArray *negativeArray = [[NSMutableArray alloc] init];
+    NSInteger result = 1;
+    for (int i = 0; i < [inputArray count]; i++) {
+        if (![[inputArray objectAtIndex:i] isKindOfClass:[NSNumber class]]) {
+            [inputArray removeObjectAtIndex:i];
+            i--;
+        } else {
+            result *= [[inputArray objectAtIndex:i] intValue];
+            if ([[inputArray objectAtIndex:i] intValue] < 0) {
+                [negativeArray addObject:[inputArray objectAtIndex:i]];
+            } else {
+                [positiveArray addObject:[inputArray objectAtIndex:i]];
+            }
+        }
+    }
+    if ([inputArray count] == 0 || numberOfItems == 0) {
+        return 0;
+    }
+    if (numberOfItems >= [inputArray count]) {
+        return result;
+    }
+    
+    [positiveArray sortUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+        return [obj2 compare:obj1];
+    }];
+    [negativeArray sortUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+        return [obj1 compare:obj2];
+    }];
+    if ([positiveArray count] > numberOfItems) {
+        [positiveArray removeObjectsInRange:NSMakeRange(numberOfItems, [positiveArray count]-numberOfItems)];
+    }
+    inputArray = [[[positiveArray reverseObjectEnumerator] allObjects] mutableCopy];
+    if ([negativeArray count] > 1) {
+        for (int i = 0; i < [negativeArray count] / 2; i++) {
+            if ([[inputArray objectAtIndex:i] intValue] * [[inputArray objectAtIndex:i+1] intValue] <
+                [[negativeArray objectAtIndex:i] intValue] * [[negativeArray objectAtIndex:i+1] intValue]) {
+                [inputArray replaceObjectAtIndex:i withObject:[negativeArray objectAtIndex:i]];
+                [inputArray replaceObjectAtIndex:i+1 withObject:[negativeArray objectAtIndex:i+1]];
+            }
+        }
+    }
+    result = 1;
+    for (int i = 0; i < [inputArray count]; i++) {
+        result *= [[inputArray objectAtIndex:i] intValue];
+    }
+    return result;
 }
 @end
--- RSSchool_T4/RSSchool_T4/3/TelephoneFinder.m
+++ RSSchool_T4/RSSchool_T4/3/TelephoneFinder.m
@@ -2,7 +2,32 @@
 
 @implementation TelephoneFinder
 - (NSArray <NSString*>*)findAllNumbersFromGivenNumber:(NSString*)number {
-    // good luck
-    return @[@"0"];
+    NSCharacterSet* notDigits = [[NSCharacterSet decimalDigitCharacterSet] invertedSet];
+    if ([number rangeOfCharacterFromSet:notDigits].location != NSNotFound)
+        return nil;
+    NSMutableString *num = [[NSMutableString alloc] initWithString:number];
+    NSMutableArray<NSString*> *answer = [NSMutableArray new];
+    NSDictionary *numbersDictionary = @{
+        @"0":@"8",
+        @"1":@"24",
+        @"2":@"135",
+        @"3":@"26",
+        @"4":@"157",
+        @"5":@"2468",
+        @"6":@"359",
+        @"7":@"48",
+        @"8":@"7590",
+        @"9":@"68"
+    };
+    for (int i = 0;i < [num length]; i++) {
+        NSString *digit = [NSString stringWithFormat:@"%c", [num characterAtIndex:i]];
+        NSString *replace = [numbersDictionary valueForKey:digit];
+        for (int j = 0; j < [replace length]; j++) {
+            [num replaceCharactersInRange:NSMakeRange(i, 1) withString:[NSString stringWithFormat:@"%c", [replace characterAtIndex:j]]];
+            [answer addObject:[num mutableCopy]];
+        }
+        num = [number mutableCopy];
+    }
+    return answer;
 }
 @end
